8f66a0b92545c102f277215516bc5c46659b0b09
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-09-10 17:09:31 +0300

    Add files via upload

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return; // empty object
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
