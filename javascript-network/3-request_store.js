2241d55dfbb739874f98400a2c1c59f998b60a32
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-09-14 14:44:07 +0300

    Update 3-request_store.js

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,43 +1,21 @@
-
-
-
 const request = require('request');
 const fs = require('fs');
 
-// Check if the correct number of arguments are provided
-if (process.argv.length !== 4) {
-  console.error('Usage: node 3-request_store.js <URL> <outputFilePath>');
-  process.exit(1);
-}
+const urlToRequest = process.argv[2];
+const filePathToStore = process.argv[3];
 
-// Get the URL and output file path from command line arguments
-const url = process.argv[2];
-const outputPath = process.argv[3];
-
-// Make an HTTP GET request to the specified URL
-request(url, (error, response, body) => {
+request.get({url: urlToRequest}, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    process.exit(1);
+    console.error(`Error while requesting ${urlToRequest}:\n${error}`);
+    return;
   }
 
-  // Check if the request was successful (status code 200)
-  if (response.statusCode === 200) {
-    // Write the response body to the specified file with UTF-8 encoding
-    fs.writeFile( body, 'utf-8', (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-        process.exit(1);
-      }
-
-      console.log( ` ${url}`);
-    });
-  } else {
-    console.error( `Failed to fetch URL (${url}), status code: ${response.statusCode}` );
-    process.exit(1);
-  }
+  // Save the body response in a file
+  fs.writeFile(filePathToStore, body, {encoding: 'utf-8'}, (error) => {
+    if (error) {
+      console.error(`Error while storing response in ${filePathToStore}:\n${error}`);
+      return;
+    }
+    console.log(`Successfully stored the response in ${filePathToStore}`);
+  });
 });
- 
-
-
-
